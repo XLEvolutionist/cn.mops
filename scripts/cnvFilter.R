e8208278afbfb541473ec1cbfc7e1f0ff88178ac
Simon Renny-Byfield <simonrenny-byfield@Jeffreys-iMac.local>
2014-12-17 18:26:19 -0800

    added a line by line check
    This required that at least 50% of the CNV overlaps with repeats before
    it is excluded.

--- scripts/cnvFilter.R
+++ scripts/cnvFilter.R
@@ -1,3 +1,14 @@
+#define a function to fileter CNVs
+#only works with apply
+lineByline<-function(x) {
+  name<-as.character(x[1])
+  start<-as.numeric(x[2])
+  end<-as.numeric(x[3])
+  minoverlap<-(end-start)/2
+  #a FLAEE answer means no overlap
+  return(overlapsAny(GRanges(seqnames = name, ranges = IRanges(start = start, end = end)), GRrep , minoverlap=minoverlap ))
+}#lineByline  
+
 #load in the cn.mops data
 load("/Users/simonrenny-byfield/bamDataRanges.RData")
 #load in the Zea repeats bed file
@@ -15,4 +26,9 @@ GRrep<-GRanges(seqnames=rep.bed$V1, ranges = IRanges(start=rep.bed$V2, end=rep.b
 hits = overlapsAny(GRcnvs,GRrep,ignore.strand = TRUE)
 countOverlaps(GRcnvs,GRrep,ignore.strand = TRUE)[1831]
 
-#We may need a really funcky apply or for loop to get the minimum overlap correct.
+# We may need a really funcky apply or for loop to get the minimum overlap correct.
+# what about making a GRanges object line by line over cnvdf and then seeing if the
+# overlap is greater than 50%
+
+screened<-cnvdf[!apply(cnvdf[1:10,],1,function(x) lineByline(x)) ,]  
+  
