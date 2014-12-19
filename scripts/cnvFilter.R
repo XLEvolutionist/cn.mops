f891705fffcdeca298be59c3a0878051454acefc
Simon Renny-Byfield <sbyfield@ucdavis.edu>
2014-12-19 12:01:20 -0800

    out out as bed file
    added a loop to print out cnvs at each frequency as a .bed file

--- scripts/cnvFilter.R
+++ scripts/cnvFilter.R
@@ -16,7 +16,7 @@ lineByline<-function(x) {
   minoverlap<-(e-s)/2
   # return FALSE if there is not sufficient overlap, TRUE if there is
   return(overlapsAny(GRanges(seqnames = name, ranges = IRanges(start = s, end = e)), GRrep , minoverlap=minoverlap ))
-  #return(overlapsAny(GRanges(seqnames = name, ranges = IRanges(start = s, end = e)), GRrep ))
+  # return(overlapsAny(GRanges(seqnames = name, ranges = IRanges(start = s, end = e)), GRrep ))
 }# lineByline  
 
 # load in the cn.mops data
@@ -26,7 +26,7 @@ rep.bed<-read.table("/Users/simonrenny-byfield/maize_genome/ZeaRefV3.bed")
 dim(cnvdf)
 # now filter the CNV calls for those that are deleted in at least one individual
 cnvdf<-cnvdf[rowSums(sapply(cnvdf, '%in%', "CN0") )>0,]
-#calculate the the number of individuals that have the CNV
+# calculate the the number of individuals that have the CNV
 cnvcp<-sapply(cnvdf, function(x) gsub("CN","",x))
 cnvcp<-matrix(as.numeric(cnvcp), ncol =dim(cnvdf)[2], byrow=FALSE)
 
@@ -35,9 +35,9 @@ cnvcp<-matrix(as.numeric(cnvcp), ncol =dim(cnvdf)[2], byrow=FALSE)
 # assessing frequency a little troubling. Maybe wou should only consider those regions
 # with max CN per individuals is 2
 
-#remove cnvs that have high more than a diploid compliment.
+# remove cnvs that have high more than a diploid compliment.
 cnvcp<-cnvcp[apply(cnvcp[,-c(1:3)],1,function(x) max(x) < 3),]
-#find the frequency of each cnv
+# find the frequency of each cnv
 frequency<-rowSums(cnvcp[,-c(1:3)])
 
 # make a genomic ranges object of the cnvs
@@ -52,5 +52,17 @@ hits = overlapsAny(GRcnvs,GRrep,ignore.strand = TRUE)
 # do it the regular way because minoverlap has to a single number, but the required
 # overlap changes with each cnv.
 
-screened<-cnvdf[!apply(cnvdf,1,function(x) lineByline(x)) ,]  
+screened<-cnvdf[!apply(cnvcp,1,function(x) lineByline(x)) ,]
+
+# now for each unique frequency, grab the appropriate CNVs
+for ( i in unique(frequency)) {
+  print(i)
+  # eventually output these CNVs as a bed file, one for each level of frequency
+  # find the index of cnvs with frequency i
+  index<-which(frequency == i)
+  # trim the table
+  out<-cnvcp[index,]
+  # write out the table
+  write.table(cnvcp[1:3,],file=paste("freq_",i,".bed"), qoute=FALSE)
+}#for
   
