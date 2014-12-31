1d851de2acb530c2b9ff633c19eeb388fdbf7bf5
Simon Renny Byfield <sbyfield@farm.cse.ucdavis.edu>
2014-12-31 02:44:28 -0800

    added loop to script

--- scripts/cnv_cnmpos.R
+++ scripts/cnv_cnmpos.R
@@ -9,23 +9,29 @@ len<-100
 library(cn.mops)
 BAMFiles <- list.files(path="/group/jrigrp4/cn.mops/data", pattern="sorted.bam$")
 setwd("/group/jrigrp4/cn.mops/data")
+
 chrs <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
-print(BAMFiles)
-bamDataRanges <- getReadCountsFromBAM(BAMFiles, refSeqName=chrs,  mode="paired",WL=len)
-
 setwd("/group/jrigrp4/cn.mops/output")
-#resHaplo <- haplocn.mops(bamDataRanges)
-#resCN <- calcIntegerCopyNumbers(resHaplo)
-# This function performs the cn.mops algorithm for copy number detection in NGS data
-res <- cn.mops(bamDataRanges)
-resCNV <- calcIntegerCopyNumbers(res)
+#for each chromosome
+for (i in chrs ) {
+	#try just chr 10 for now
+	#chrs<-10
+	#print(BAMFiles)
+	bamDataRanges <- getReadCountsFromBAM(BAMFiles, refSeqName=i,  mode="paired",WL=len)
 
-### transform GRanges to data.frame
-mycnv <- cnvr(resCNV)
+	#resHaplo <- haplocn.mops(bamDataRanges)
+	#resCN <- calcIntegerCopyNumbers(resHaplo)
+	# This function performs the cn.mops algorithm for copy number detection in NGS data
+	res <- cn.mops(bamDataRanges)
+	resCNV <- calcIntegerCopyNumbers(res)
+
+	### transform GRanges to data.frame
+	mycnv <- cnvr(resCNV)
 
 
-cnvdf <- data.frame(chr=as.character(seqnames(mycnv)), start=start(mycnv), end=end(mycnv))
-callcnv <- mcols(mycnv)
-cnvdf <- cbind(cnvdf, callcnv)
+	cnvdf <- data.frame(chr=as.character(seqnames(mycnv)), start=start(mycnv), end=end(mycnv))
+	callcnv <- mcols(mycnv)
+	cnvdf <- cbind(cnvdf, callcnv)
 
-save(file=paste("bamDataRanges", len, ".RData", list=c("bamDataRanges","cnvdf") )
+	save(file=paste("bamDataRanges_", i, ".RData", list=c("bamDataRanges","cnvdf") )
+}#for
